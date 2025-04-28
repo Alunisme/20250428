b297a534af01a7a7b01aab38cdde0f85689845bb
Alunisme <413730267@o365.tku.edu.tw>
2025-04-28 10:09:05 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,6 +10,8 @@ function setup() {
 
 function draw() {
   background('#5CADAD');
+  translate(width, 0); // 移動畫布的原點到右上角
+  scale(-1, 1); // 水平翻轉畫布
   image(capture, (width - capture.width) / 2, (height - capture.height) / 2);
 }
 
