821399e692e212c23be57426adbf319204d85e43
Alunisme <413730267@o365.tku.edu.tw>
2025-04-28 10:44:40 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -10,9 +10,7 @@ function setup() {
 
   // 建立與視訊畫面相同大小的 Graphics
   overlayGraphics = createGraphics(capture.width, capture.height);
-  overlayGraphics.fill(255, 0, 0, 150); // 半透明紅色
-  overlayGraphics.noStroke();
-  overlayGraphics.ellipse(overlayGraphics.width / 2, overlayGraphics.height / 2, 100, 100); // 畫一個圓
+  drawOverlayGraphics();
 }
 
 function draw() {
@@ -37,7 +35,20 @@ function windowResized() {
 
   // 更新 overlayGraphics 的大小
   overlayGraphics = createGraphics(capture.width, capture.height);
-  overlayGraphics.fill(255, 0, 0, 150); // 半透明紅色
+  drawOverlayGraphics();
+}
+
+function drawOverlayGraphics() {
+  overlayGraphics.background(0); // 設定背景為黑色
   overlayGraphics.noStroke();
-  overlayGraphics.ellipse(overlayGraphics.width / 2, overlayGraphics.height / 2, 100, 100); // 畫一個圓
+
+  // 每隔 20 繪製一個圓
+  for (let x = 10; x < overlayGraphics.width; x += 20) {
+    for (let y = 10; y < overlayGraphics.height; y += 20) {
+      // 從 capture 中取得相對應位置的顏色
+      let col = capture.get(x, y);
+      overlayGraphics.fill(col);
+      overlayGraphics.ellipse(x, y, 15, 15); // 繪製圓形
+    }
+  }
 }
